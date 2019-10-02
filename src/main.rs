7e2cf526e9c20c59ab4422b92257944a8e0f29cc
Karsten Jeschkies <k@jeschkies.xyz>
2019-10-02 10:26:28 +0200

    Parse arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.as_slice() {
+        [_, file] => runFile(file),
+        [_] => runPrompt(),
+        _ => {
+            eprintln!("Usage: lox-rs [script]");
+            exit(64)
+        }
+    }
+}
+
+fn runFile(path: &str) {
+    println!("Run file {}", path)
+}
+
+fn runPrompt() {
+    println!("run prompt")
 }
