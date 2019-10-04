a99a1a9ee3930713e08e162f31b403f4dbe085fd
Karsten Jeschkies <k@jeschkies.xyz>
2019-10-04 16:13:06 +0200

    Read input.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-use std::env;
+use std::{env, fs};
+use std::io::{self, BufRead};
 use std::process::exit;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let args: Vec<String> = env::args().collect();
     match args.as_slice() {
-        [_, file] => runFile(file),
-        [_] => runPrompt(),
+        [_, file] => run_file(file)?,
+        [_] => run_prompt()?,
         _ => {
             eprintln!("Usage: lox-rs [script]");
             exit(64)
         }
     }
+    Ok(())
 }
 
-fn runFile(path: &str) {
-    println!("Run file {}", path)
+fn run_file(path: &str) -> io::Result<()> {
+    let source = fs::read_to_string(path)?;
+    run(source)
 }
 
-fn runPrompt() {
-    println!("run prompt")
+fn run_prompt() -> io::Result<()> {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        run(line?)?;
+        print!("> ");
+    }
+    Ok(())
+}
+
+fn run(source: String) -> io::Result<()> {
+    scanner = Scanner::new(source);
+    let tokens = scanner.scan_tokesn();
+
+    for token in tokens {
+        println!("{}", token);
+    }
+    Ok(())
+}
+
+fn error(line: i32, message: String) {
+    report(line, "".to_string(), message);
+}
+
+fn report(line: i32, where_: String, message: String) {
+    eprintln!("[line {}] Error{}: {}", line, where_, message);
+    // had_error = true;
 }
