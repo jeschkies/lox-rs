5db0302bc953f31197e7f00151077b7213867bbd
Karsten Jeschkies <k@jeschkies.xyz>
2019-11-06 18:02:54 +0100

    Propagate error to `parse`.

--- src/parser.rs
+++ src/parser.rs
@@ -26,16 +26,20 @@ impl Parser {
         Parser { tokens, current: 0 }
     }
 
-    fn expression(&mut self) -> Expr {
+    fn parse(&mut self) -> Expr {
+        self.expression().expect("TODO: Handle parse error.")
+    }
+
+    fn expression(&mut self) -> Result<Expr, Error> {
         self.equality()
     }
 
-    fn equality(&mut self) -> Expr {
-        let mut expr = self.comparison();
+    fn equality(&mut self) -> Result<Expr, Error> {
+        let mut expr = self.comparison()?;
 
         while matches!(self, TokenType::BangEqual, TokenType::EqualEqual) {
             let operator: Token = (*self.previous()).clone();
-            let right: Expr = self.comparison();
+            let right: Expr = self.comparison()?;
             expr = Expr::Binary {
                 left: Box::new(expr),
                 operator,
@@ -43,7 +47,7 @@ impl Parser {
             };
         }
 
-        expr
+        Ok(expr)
     }
 
     fn check(&self, token_type: TokenType) -> bool {
@@ -112,8 +116,8 @@ impl Parser {
         self.peek().tpe == TokenType::EOF
     }
 
-    fn comparison(&mut self) -> Expr {
-        let mut expr = self.addition();
+    fn comparison(&mut self) -> Result<Expr, Error> {
+        let mut expr = self.addition()?;
 
         while matches!(
             self,
@@ -123,7 +127,7 @@ impl Parser {
             TokenType::LessEqual
         ) {
             let operator: Token = self.previous().clone();
-            let right = self.addition();
+            let right = self.addition()?;
             expr = Expr::Binary {
                 left: Box::new(expr),
                 operator,
@@ -131,15 +135,15 @@ impl Parser {
             }
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn addition(&mut self) -> Expr {
-        let mut expr = self.multiplication();
+    fn addition(&mut self) -> Result<Expr, Error> {
+        let mut expr = self.multiplication()?;
 
         while matches!(self, TokenType::Minus, TokenType::Plus) {
             let operator: Token = self.previous().clone();
-            let right = self.multiplication();
+            let right = self.multiplication()?;
             expr = Expr::Binary {
                 left: Box::new(expr),
                 operator,
@@ -147,15 +151,15 @@ impl Parser {
             }
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn multiplication(&mut self) -> Expr {
-        let mut expr = self.unary();
+    fn multiplication(&mut self) -> Result<Expr, Error> {
+        let mut expr = self.unary()?;
 
         while matches!(self, TokenType::Slash, TokenType::Star) {
             let operator: Token = self.previous().clone();
-            let right = self.unary();
+            let right = self.unary()?;
             expr = Expr::Binary {
                 left: Box::new(expr),
                 operator,
@@ -163,55 +167,52 @@ impl Parser {
             }
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn unary(&mut self) -> Expr {
+    fn unary(&mut self) -> Result<Expr, Error> {
         if matches!(self, TokenType::Bang, TokenType::Minus) {
             let operator: Token = self.previous().clone();
-            let right = self.unary();
-            Expr::Unary {
+            let right = self.unary()?;
+            Ok(Expr::Unary {
                 operator,
                 right: Box::new(right),
-            }
+            })
         } else {
             self.primary()
         }
     }
 
-    fn primary(&mut self) -> Expr {
+    fn primary(&mut self) -> Result<Expr, Error> {
         // We don't use matches!() here since we want to extract the literals.
-        if !self.is_at_end() {
-            let expr = match &self.peek().tpe {
-                TokenType::False => Expr::Literal {
-                    value: LiteralValue::Boolean(false),
-                },
-                TokenType::True => Expr::Literal {
-                    value: LiteralValue::Boolean(true),
-                },
-                TokenType::Nil => Expr::Literal {
-                    value: LiteralValue::Null,
-                },
-                TokenType::String { literal } => Expr::Literal {
-                    value: LiteralValue::String(literal.clone()),
-                },
-                TokenType::Number { literal } => Expr::Literal {
-                    value: LiteralValue::Number(literal.clone()),
-                },
-                TokenType::LeftParen => {
-                    let expr = self.expression();
-                    self.consume(TokenType::RightParen, "Expected ')' after expression.")
-                        .expect("Unhandled error"); // TODO: handle result
-                    Expr::Grouping {
-                        expression: Box::new(expr),
-                    }
+        let expr = match &self.peek().tpe {
+            TokenType::False => Expr::Literal {
+                value: LiteralValue::Boolean(false),
+            },
+            TokenType::True => Expr::Literal {
+                value: LiteralValue::Boolean(true),
+            },
+            TokenType::Nil => Expr::Literal {
+                value: LiteralValue::Null,
+            },
+            TokenType::String { literal } => Expr::Literal {
+                value: LiteralValue::String(literal.clone()),
+            },
+            TokenType::Number { literal } => Expr::Literal {
+                value: LiteralValue::Number(literal.clone()),
+            },
+            TokenType::LeftParen => {
+                let expr = self.expression()?;
+                self.consume(TokenType::RightParen, "Expected ')' after expression.")?;
+                Expr::Grouping {
+                    expression: Box::new(expr),
                 }
-                _ => panic!("Unexpected token"),
-            };
-            self.advance();
-            expr
-        } else {
-            panic!("Unexpected end.")
-        }
+            }
+            _ => return Err(self.error(self.peek(), "Expect expression.")),
+        };
+
+        self.advance();
+
+        Ok(expr)
     }
 }
