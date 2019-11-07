79fde1792a0fbcd2e47dce1ce3a1ead8550c7a4a
Karsten Jeschkies <k@jeschkies.xyz>
2019-11-07 15:34:01 +0100

    Support string concat.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -26,10 +26,29 @@ impl Interpreter {
 }
 
 impl Visitor<Object> for Interpreter {
-    // TODO: Book defines result type as Object
 
     fn visit_binary_expr(&self, left: &Expr, operator: &Token, right: &Expr) -> Object {
-        unimplemented!()
+        let l = self.evaluate(left);
+        let r = self.evaluate(right);
+
+        match (l, &operator.tpe, r) {
+            (Object::Number(left_number), TokenType::Minus, Object::Number(right_number)) => {
+                Object::Number(left_number - right_number)
+            }
+            (Object::Number(left_number), TokenType::Plus, Object::Number(right_number)) => {
+                Object::Number(left_number + right_number)
+            }
+            (Object::String(left_string), TokenType::Plus, Object::String(right_string)) => {
+                Object::String(left_string.clone() + &right_string)
+            }
+            (Object::Number(left_number), TokenType::Slash, Object::Number(right_number)) => {
+                Object::Number(left_number / right_number)
+            }
+            (Object::Number(left_number), TokenType::Star, Object::Number(right_number)) => {
+                Object::Number(left_number * right_number)
+            }
+            _ => unreachable!(), // TODO: handle other types
+        }
     }
 
     fn visit_grouping_expr(&self, expr: &Expr) -> Object {
