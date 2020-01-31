4a26e77f4bb5d8f8e9233f4359773f8ab18462b4
Karsten Jeschkies <k@jeschkies.xyz>
2020-01-31 11:52:39 +0100

    Scan all identifiers.

--- bytecode/src/scanner.rs
+++ bytecode/src/scanner.rs
@@ -98,6 +98,22 @@ impl<'a> Scanner<'a> {
         self.char_at(self.current)
     }
 
+    fn is_alpha(&self, c: char) -> bool {
+        match c {
+            'a'..='z' => true,
+            'A'..='Z' => true,
+            '_' => true,
+            _ => false,
+        }
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        match c {
+            '0'..='9' => true,
+            _ => false,
+        }
+    }
+
     fn is_at_end(&self) -> bool {
         self.char_at(self.current) == '\0'
     }
@@ -176,6 +192,99 @@ impl<'a> Scanner<'a> {
         }
     }
 
+    fn check_keyword(&self, start: usize, length: usize, rest: &str, typ: TokenType) -> TokenType {
+        let left = &self.source[(self.start + start)..(self.start + start + length)];
+        if self.current - self.start == start + length && left == rest {
+            typ
+        } else {
+            TokenType::Identifier
+        }
+    }
+
+    fn identifier_type(&mut self) -> TokenType {
+        match self.char_at(self.start) {
+            'a' => self.check_keyword(1, 2, "nd", TokenType::And),
+            'c' => self.check_keyword(1, 4, "lass", TokenType::Class),
+            'e' => self.check_keyword(1, 3, "lse", TokenType::Else),
+            'f' => {
+                if self.current - self.start > 1 {
+                    match self.char_at(self.start + 1) {
+                        'a' => self.check_keyword(2, 3, "lse", TokenType::False),
+                        'o' => self.check_keyword(2, 1, "r", TokenType::For),
+                        'u' => self.check_keyword(2, 1, "n", TokenType::Fun),
+                        _ => TokenType::Identifier,
+                    }
+                } else {
+                    TokenType::Identifier
+                }
+            }
+            'i' => self.check_keyword(1, 1, "f", TokenType::If),
+            'n' => self.check_keyword(1, 2, "il", TokenType::Nil),
+            'o' => self.check_keyword(1, 1, "r", TokenType::Or),
+            'p' => self.check_keyword(1, 4, "rint", TokenType::Print),
+            'r' => self.check_keyword(1, 5, "eturn", TokenType::Return),
+            's' => self.check_keyword(1, 4, "uper", TokenType::Super),
+            't' => {
+                if self.current - self.start > 1 {
+                    match self.char_at(self.start + 1) {
+                        'h' => self.check_keyword(2, 2, "is", TokenType::This),
+                        'r' => self.check_keyword(2, 2, "ue", TokenType::True),
+                        _ => TokenType::Identifier,
+                    }
+                } else {
+                    TokenType::Identifier
+                }
+            }
+            'v' => self.check_keyword(1, 2, "ar", TokenType::Var),
+            'w' => self.check_keyword(1, 4, "hile", TokenType::While),
+            _ => TokenType::Identifier,
+        }
+    }
+
+    fn identifier(&mut self) -> Token {
+        while self.is_alpha(self.peek()) || self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        let typ = self.identifier_type();
+        self.make_token(typ)
+    }
+
+    fn number(&mut self) -> Token {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        // Look for a fractional part.
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            // Consume the "."
+            self.advance();
+
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        self.make_token(TokenType::Number)
+    }
+
+    fn string(&mut self) -> Token {
+        while self.peek() != '=' && !self.is_at_end() {
+            if self.peek() != '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return self.error_token("Unterminated string.");
+        }
+
+        // The closing quote.
+        self.advance();
+        self.make_token(TokenType::String)
+    }
+
     pub fn scan_token(&mut self) -> Token {
         self.skip_whitespace();
 
@@ -186,6 +295,13 @@ impl<'a> Scanner<'a> {
         }
 
         let c: char = self.advance();
+
+        if self.is_alpha(c) {
+            return self.identifier();
+        }
+        if self.is_digit(c) {
+            return self.number();
+        }
 
         match c {
             '(' => return self.make_token(TokenType::LeftParen),
@@ -231,9 +347,39 @@ impl<'a> Scanner<'a> {
                 };
                 return self.make_token(typ);
             }
+            '=' => return self.string(),
             _ => unimplemented!(),
         }
 
         return self.error_token("Unexpected character.");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn scan_token_type() {
+        let mut scanner = Scanner::new(
+            "and class else false\r fun \nfor if nil or print return super this true var while\0",
+        );
+
+        assert_eq!(scanner.scan_token().typ, TokenType::And);
+        assert_eq!(scanner.scan_token().typ, TokenType::Class);
+        assert_eq!(scanner.scan_token().typ, TokenType::Else);
+        assert_eq!(scanner.scan_token().typ, TokenType::False);
+        assert_eq!(scanner.scan_token().typ, TokenType::Fun);
+        assert_eq!(scanner.scan_token().typ, TokenType::For);
+        assert_eq!(scanner.scan_token().typ, TokenType::If);
+        assert_eq!(scanner.scan_token().typ, TokenType::Nil);
+        assert_eq!(scanner.scan_token().typ, TokenType::Or);
+        assert_eq!(scanner.scan_token().typ, TokenType::Print);
+        assert_eq!(scanner.scan_token().typ, TokenType::Return);
+        assert_eq!(scanner.scan_token().typ, TokenType::Super);
+        assert_eq!(scanner.scan_token().typ, TokenType::This);
+        assert_eq!(scanner.scan_token().typ, TokenType::True);
+        assert_eq!(scanner.scan_token().typ, TokenType::Var);
+        assert_eq!(scanner.scan_token().typ, TokenType::While);
     }
 }
